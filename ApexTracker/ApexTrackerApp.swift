b3a234f6434263c55b0951abea075393ba4ea10b
Takaki Hamahiga <2tf2003@gmail.com>
2021-07-20 09:24:32 +0900

    initialize

---
+++ ApexTracker/ApexTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApexTrackerApp.swift
+//  ApexTracker
+//
+//  Created by 2tf on 2021/07/20.
+//
+
+import SwiftUI
+
+@main
+struct ApexTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApexTracker/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  ApexTracker
+//
+//  Created by 2tf on 2021/07/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice("iPhone 12")
+    }
+}
